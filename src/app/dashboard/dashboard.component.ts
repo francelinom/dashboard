3494be08926e554b9d2cf3c3be1c3aed17c4cc4a
francelino <fmarcilio1989@gmail.com>
2022-04-22 09:09:41 -0300

    metodos para exibir os gráficos

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,65 @@
+import { DadosService } from './dados.service';
 import { Component, OnInit } from '@angular/core';
+
+declare var google: any;
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  private dados: any;
 
-  constructor() { }
+  constructor(private dadosService: DadosService) {}
 
   ngOnInit(): void {
+    this.dadosService.obterDados().subscribe((dados) => {
+      this.dados = dados;
+      this.init();
+    });
   }
 
+  init(): void {
+    if (typeof google !== 'undefined') {
+      google.charts.load('current', { packages: ['corechart'] });
+      setTimeout(() => {
+        google.charts.setOnLoadCallback(this.exibirGraficos());
+      }, 1000);
+    }
+  }
+
+  exibirGraficos(): void {
+    this.exibirPieChart();
+    // this.exibir3dPieChart();
+    // this.exibirBarChart();
+    // this.exibirLineChart();
+    // this.exibirColumnChart();
+    // this.exibirDonutChart();
+  }
+
+  exibirPieChart(): void {
+    const el = document.getElementById('pie_chart');
+    const chart = new google.visualization.PieChart(el);
+
+    chart.draw(this.obterDataTable(), this.obterOpcoes());
+  }
+
+  obterDataTable(): any {
+    const data = new google.visualization.DataTable();
+
+    data.addColumn('string', 'Mês');
+    data.addColumn('number', 'Quantidade');
+    data.addRows(this.dados);
+
+    return data;
+  }
+
+  obterOpcoes(): any {
+    return {
+      title: 'Quantidade de cadastros primeiro semestre',
+      width: 400,
+      height: 300,
+    };
+  }
 }
